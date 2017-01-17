2cbb4ce5148adaa53f3772afe9ea06c2c8a3da0a
= <seimith@gmail.com>
2017-01-16 23:13:31 -0800

    Add test car class;

--- RedBlockCarthageFramework/ViewController.swift
+++ RedBlockCarthageFramework/ViewController.swift
@@ -28,3 +28,23 @@ public class ViewController: UIViewController {
 
 }
 
+
+public class Car: CustomStringConvertible {
+    
+    var name: String
+    var miles: Int
+    
+    public init(name: String, miles: Int) {
+        self.name = name
+        self.miles = miles
+    }
+    
+    public func addMiles(miles: Int) {
+        self.miles += miles
+    }
+    
+    public var description: String {
+        return "Car '\(name)' has \(miles) miles."
+    }
+    
+}
