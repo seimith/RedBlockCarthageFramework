a1f276e1818edcf0b22e603a5d23ff95700ddc1a
= <seimith@gmail.com>
2017-01-16 22:15:54 -0800

    Test Carthage framework - red block

--- RedBlockCarthageFramework/ViewController.swift
+++ RedBlockCarthageFramework/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let redBlockUIView = UIView()
+        redBlockUIView.backgroundColor = UIColor.red
+        redBlockUIView.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        self.view.addSubview(redBlockUIView)
+        
     }
 
     override func didReceiveMemoryWarning() {
