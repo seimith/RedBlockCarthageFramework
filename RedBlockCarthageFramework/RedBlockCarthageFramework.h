fb616a25de6dcc760f0b206d17debd33ee67c622
= <seimith@gmail.com>
2017-01-26 00:45:39 -0800

    Car test

---
+++ RedBlockCarthageFramework/RedBlockCarthageFramework.h
@@ -0,0 +1,19 @@
+//
+//  RedBlockCarthageFramework.h
+//  RedBlockCarthageFramework
+//
+//  Created by Seimith on 1/26/17.
+//  Copyright © 2017 Seimith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RedBlockCarthageFramework.
+FOUNDATION_EXPORT double RedBlockCarthageFrameworkVersionNumber;
+
+//! Project version string for RedBlockCarthageFramework.
+FOUNDATION_EXPORT const unsigned char RedBlockCarthageFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RedBlockCarthageFramework/PublicHeader.h>
+
+
---
+++ RedBlockCarthageFrameworkiOS/RedBlockCarthageFrameworkiOS.h
@@ -0,0 +1,19 @@
+//
+//  RedBlockCarthageFrameworkiOS.h
+//  RedBlockCarthageFrameworkiOS
+//
+//  Created by Seimith on 1/26/17.
+//  Copyright © 2017 Seimith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RedBlockCarthageFrameworkiOS.
+FOUNDATION_EXPORT double RedBlockCarthageFrameworkiOSVersionNumber;
+
+//! Project version string for RedBlockCarthageFrameworkiOS.
+FOUNDATION_EXPORT const unsigned char RedBlockCarthageFrameworkiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RedBlockCarthageFrameworkiOS/PublicHeader.h>
+
+
